dc2242f04533434e5e4075e3225d9f2acca49bf8
Emma Harris <harris.aact@gmail.com>
2019-07-24 21:24:18 -0700

    burger test

--- _site/scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-//Add all jQuery in here
-
-$( document ).ready(function() {
- $(".burger-button").click(function(){
-   $(".burger-button").toggleClass("active");
-   $(".burger-menu").toggleClass("active");
- });
-});
